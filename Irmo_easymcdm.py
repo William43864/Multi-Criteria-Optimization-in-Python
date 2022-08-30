1c744e8a68069d33b65e33e5f7de1a73ae46b4ee
Keivan Tafakkori <91466697+ktafakkori@users.noreply.github.com>
2022-08-30 16:51:01 +0430

    Update and rename IRMO_easymcdm.py to Irmo_easymcdm.py

---
+++ Irmo_easymcdm.py
@@ -0,0 +1,37 @@
+#installation (outcomment the line below)
+#!pip install easymcdm
+
+from EasyMCDM.models.Irmo import Irmo
+
+#============================================================================#
+#Ensure to credit the original developer:
+#Platform: https://github.com/ktafakkori
+#Repository: Multi-Criteria-Optimization-in-Python
+#File: Irmo_easymcdm.py
+#Developer: © Keivan Tafakkori 
+#Year: 2022
+#Month: August
+#Day: 30
+#Source: https://github.com/ktafakkori/Multi-Criteria-Optimization-in-Python
+#============================================================================#
+
+DecisionMatrix = {
+    #      C1  C2  C3
+    "A1": [80, 90, 600],
+    "A2": [65, 58, 200],
+    "A3": [83, 60, 400]
+}
+
+direction = ["min", "max", "min"]
+
+p = Irmo(data=DecisionMatrix, verbose=False)
+res = p.solve(
+    indexes=[0, 1, 2],
+    prefs=direction
+)
+print(res)
+
+#Output:
+'''
+{'best': 'A1', 'eleminated': ['A1', 'A2', 'A3']}
+'''  
